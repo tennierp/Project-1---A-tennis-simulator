7e536a2350ceabff9f1ea4a8e23b05bf7173e0e3
Parker Tennier <227867231+tennierp@users.noreply.github.com>
2026-09-09 17:06:08 -0700

    Update Score TieBreaker::play(Player *firstServer) function

--- TieBreaker.cpp
+++ TieBreaker.cpp
@@ -1,5 +1,9 @@
 #include "TieBreaker.hpp"
 
+#include "Player.hpp"
+#include "PointScore.hpp"
+#include "TieBreakerScore.hpp"
+
 TieBreaker::TieBreaker(Player *player1, Player *player2)
     : Competition(player1, player2)
 {
@@ -9,6 +13,27 @@ Score *TieBreaker::play(Player *firstServer)
 {
     // TODO: Play points in the required service order until TieBreakerScore
     // reports a winner. Return an owning pointer to that score.
-    (void)firstServer;
-    return nullptr;
+    // (void)firstServer;
+    // return nullptr;
+
+    TieBreakerScore *tieBreakerScore = new TieBreakerScore(player1(), player2());
+    Player *server = firstServer;
+    Player *opponent = otherPlayer(firstServer);
+    int currentServersTurns = 1;
+
+    while (!tieBreakerScore->haveAWinner()) {
+        PointScore pointScore(player1(), player2());
+        pointScore.addScore(server->serveAPoint(opponent));
+        tieBreakerScore->addScore(pointScore.getWinner());
+        currentServersTurns++;
+
+        if (currentServersTurns == 2) {
+            Player *temp = server;
+            server = opponent;
+            opponent = temp;
+            currentServersTurns = 0;
+        }
+    }
+
+    return tieBreakerScore;
 }
--- TieBreakerScore.cpp
+++ TieBreakerScore.cpp
@@ -10,10 +10,12 @@ TieBreakerScore::TieBreakerScore(Player *player1, Player *player2)
 
 // TODO: Define the two overrides declared in TieBreakerScore.hpp.
 
+// If player1 or player2 has a score above 7, and their score is above the losing opponent by 2, return true.
 bool TieBreakerScore::haveAWinner() const {
     return (p1Score >= 7 || p2Score >= 7) && std::abs(p1Score - p2Score) >= 2;
 }
 
+// Print the TieBreakerScore player1Score and player2Score while using the TieBreakerScore's print()
 void TieBreakerScore::print() const {
     std::cout << "TieBreakerScore::print begins\n"
               << "Player A score = " << player1Score() << '\n'
